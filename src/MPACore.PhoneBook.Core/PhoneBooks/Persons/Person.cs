f919fdab9a6f923cae84e2f867d3c01dd6e99ab5
王雄泼 <1109351409@qq.com>
2018-07-17 17:08:27 +0800

    添加项目文件。

---
+++ src/MPACore.PhoneBook.Core/PhoneBooks/Persons/Person.cs
@@ -0,0 +1,13 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MPACore.PhoneBook.PhoneBooks.Persons
+{
+    public class Person : FullAuditedEntity
+    {
+        public string Name { get; set; }
+
+    }
+}
